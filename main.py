b2e4252b4ba4c97d3de7441ce7a4a3b597c3a66a
iceloX <icelox@qq.com>
2023-02-06 00:03:48 +0800

    优化python代码

--- main.py
+++ main.py
@@ -9,14 +9,17 @@ class GptApi:
     def __init__(self):
         pass
 
-    def getGPT(self, prompt):
-        res = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=prompt.replace("@ChatGPT", ""),
-            temperature=0.7,
-            max_tokens=3000,
-            top_p=1,
-            frequency_penalty=0,
-            presence_penalty=0
-        )
-        return res.choices[0].text
+    def getGPT(self, prompt: str):
+        if prompt is None:
+            return ""
+        else:
+            res = openai.Completion.create(
+                model="text-davinci-003",
+                prompt=prompt,
+                temperature=0.7,
+                max_tokens=3000,
+                top_p=1,
+                frequency_penalty=0,
+                presence_penalty=0
+            )
+            return res.choices[0].text
